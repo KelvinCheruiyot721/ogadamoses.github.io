d35335bcee4619dd0c48a24c2dcd13b32ad5cc45
Ogada Moses <ogadamoses59@gmail.com>
2022-09-28 08:07:59 +0300

    Add ability to change the gallery Image

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,16 @@
 function activateGallery() {
-    alert("Hello from the gallery file");
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    let mainImage = document.querySelector("#gallery-photo img");
+
+    thumbnails.forEach(function(thumbnail) {
+        thumbnail.addEventListener("click", function() {
+            // Set  Clicked Image as Main Page
+            let newImageSrc = thumbnail.dataset.largeVersion;
+            mainImage.setAttribute("src", newImageSrc);
+           
+            // Change current Image
+            document.querySelector(".current").classList.remove("current");
+            thumbnail.parentNode.classList.add("current");
+        });
+    });
 }
